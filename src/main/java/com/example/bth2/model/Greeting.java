dedf81471411f024b289baee713ebe6eac2d6b69
Trần Phúc Hưng - 0147 <121912953+phuchung123@users.noreply.github.com>
2024-12-21 16:16:24 +0700

    Delete src directory

--- src/main/java/com/example/bth2/model/Greeting.java
+++
@@ -1,3 +0,0 @@
-package com.example.bth2.model;
-
-public record Greeting(long id, String content) { }
--- src/test/java/com/bth/sao/model/Greeting.java
+++
@@ -1,3 +0,0 @@
-package com.bth.sao.model;
-
-//public record Greeting(long id, String content) { }
