e8e6b1840025b4697de0fac667f55ae0df152521
Trần Phúc Hưng - 0147 <121912953+phuchung123@users.noreply.github.com>
2024-12-21 16:21:33 +0700

    Add files via upload

---
+++ src/test/java/com/bth/sao/SaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bth.sao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
