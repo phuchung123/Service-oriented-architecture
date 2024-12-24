27a413b2afd57d824969de81eaf0480b00dd29b5
phuchung123 <tranhung.2612va@gmail.com>
2024-12-24 15:20:58 +0700

    Bai1
    Bai1 Update

---
+++ Bth1/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bth1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
