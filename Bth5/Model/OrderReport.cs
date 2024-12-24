872c6162ec91ddb29314975a200a203c44a07170
phuchung123 <tranhung.2612va@gmail.com>
2024-12-24 17:27:37 +0700

    Bai5
    Bai5 Update

---
+++ Bth5/Model/OrderReport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bth5.Models
+{
+    [Table("order_reports")]
+    public class OrderReport
+    {
+        [Key]
+        public int Identifier { get; set; }
+
+        public int OrderIdentifier { get; set; }
+
+        public decimal Revenue { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public decimal Profit { get; set; }
+    }
+}
