fc6ea5e7d4fc8e9358e4aa66c4af23936946bfe2
phuchung123 <tranhung.2612va@gmail.com>
2024-12-24 16:43:26 +0700

    Bai3
    Bai3 Update

---
+++ Bth3/Model/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bth3.Models
+{
+    [Table("products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ProductName { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        [Required]
+        public decimal ProductPrice { get; set; }
+
+        [Required]
+        public int ProductQuantity { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedDate { get; set; }
+
+        [Column("updated_at")]
+        public DateTime UpdatedDate { get; set; }
+    }
+}
